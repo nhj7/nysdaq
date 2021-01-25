c2f997d0636bfd5da0fe13f47d05a41a68eff29f
nhj12311 <nhj12311@gmail.com>
2021-01-26 01:49:33 +0900

    update for

--- insert_stock_history.js
+++ insert_stock_history.js
@@ -4,16 +4,49 @@ fs.readdir('./', 'utf8', function(err, dirList){
     console.table((dirList));
 });
 
-var iconv = require('iconv-lite');
+const iconv = require('iconv-lite');
+const cheerio = require('cheerio');
 
+const getItemValue = (node, childIdx) => {
 
+  const rtnVal = node 
+  && node.children[childIdx] 
+  && node.children[childIdx].children[0]
+  && node.children[childIdx].children[0].nodeValue.trim();
+  return rtnVal ? rtnVal : "";
+}
 
 (async () =>{
   console.log("async start");
 
   fs.readFile("상장법인목록.xls", "latin1", (err, data) => {
     var utfData = iconv.decode(data, "EUC-KR")
-    console.log(utfData);
+    //console.log(utfData);
+
+    const $ = cheerio.load(utfData);
+    const arrItem = $("html>body>table>tbody>tr");
+    //debugger;
+    arrItem.each((idx, el) => {
+      try {
+        console.log(
+          idx
+          , getItemValue(el, 1)
+          , getItemValue(el, 3)
+          , getItemValue(el, 5)
+          , getItemValue(el, 7)
+          , getItemValue(el, 9)
+          , getItemValue(el, 11)
+          , getItemValue(el, 13)
+          , getItemValue(el, 15)
+          , getItemValue(el, 17)
+        );
+      } catch (error) {
+        console.error(idx, error);
+      }
+      
+    });
+    
+    debugger;
   })
 
 })();
