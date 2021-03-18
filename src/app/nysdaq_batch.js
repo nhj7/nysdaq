e49a889f84cebc2e33c3ab7727b3603a21149715
nhj12311 <nhj12311@gmail.com>
2021-03-18 21:34:22 +0900

    add batch option manual run.

--- src/app/nysdaq_batch.js
+++ src/app/nysdaq_batch.js
@@ -5,18 +5,52 @@ const db = require("../libs/db")
 log.debug("batch start");
 
 const main = async () => {
+
     const workHistQry = `INSERT INTO nysdaq.TB_WORK_H ( WORK_TITLE, WORK_STATUS, WORK_CONTENT )
     VALUES ( 'DAILY_BATCH', '0', '')`
     await db.pool.query(workHistQry);
+
     await batch.insertStockItem();
     await batch.insertStockDayPrice();
+
+    log.debug('end main manual');
+
 }
 
-const cron = require('node-cron');
-cron.schedule('0 31 */1 * * *', async () => {
-//cron.schedule('0 */1 * * * *', async () => {    
-    log.debug('running a task every day 1 16 hour ');
-    await main();
-    log.debug('end.');}
-    , { timezone: "Asia/Seoul" }
-);
+if (process.argv[2] == "run") {
+    log.debug('start main manual');
+    main();
+    return;
+}
+
+if (process.argv[2] != "run") {
+    const cron = require('node-cron');
+
+    // every day am 8 3: 0
+    cron.schedule('0 30 8 */1 * *', async () => {
+        //cron.schedule('0 */1 * * * *', async () => {    
+        log.debug('insertStockItem start');
+
+        const workHistQry = `INSERT INTO nysdaq.TB_WORK_H ( WORK_TITLE, WORK_STATUS, WORK_CONTENT )
+        VALUES ( 'BATCH', '0', 'insertStockItem')`
+        await db.pool.query(workHistQry);
+
+        await batch.insertStockItem();
+        log.debug('end.');
+    }
+        , { timezone: "Asia/Seoul" }
+    );
+
+    cron.schedule('0 31 */1 * * *', async () => {
+        //cron.schedule('0 */1 * * * *', async () => {
+        const workHistQry = `INSERT INTO nysdaq.TB_WORK_H ( WORK_TITLE, WORK_STATUS, WORK_CONTENT )
+            VALUES ( 'BATCH', '0', 'insertStockDayPrice')`
+        await db.pool.query(workHistQry);
+        log.debug('insertStockDayPrice start ');
+        await batch.insertStockDayPrice();
+        log.debug('end.');
+    }
+
+        , { timezone: "Asia/Seoul" }
+    );
+}
